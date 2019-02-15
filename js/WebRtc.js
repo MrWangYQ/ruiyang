986fb31f47375a0f7d460859ce3ce9a610bb8f73
wangyaqiang <wangyaqiang@51talk.com>
2019-02-15 10:57:29 +0800

    feat：新增audio video

---
+++ js/WebRtc.js
@@ -0,0 +1,79 @@
+'use strict';
+
+function ShowRtc() {
+    console.log('rtc');
+}
+
+
+
+
+// 读取相关 DOM 结构
+let videoElement = document.getElementById('WebRtcVideo');
+let webRtcTips = document.getElementsByClassName('tips')[0];
+let audioSelect = document.querySelector('#audioSource');
+let videoSelect = document.querySelector('#videoSource');
+
+// 切换选项更新 音频 视频流
+audioSelect.onchange = getStream;
+videoSelect.onchange = getStream;
+
+
+// 读取设备 音频 视频 配置
+navigator.mediaDevices.enumerateDevices()
+    .then(getDevices).then(getStream).catch(handleError);
+
+function getDevices(deviceInfos) {
+    for (var i = 0; i !== deviceInfos.length; ++i) {
+        let deviceInfo = deviceInfos[i];
+        let option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === 'audioinput') {
+            const audioLable = deviceInfo.label ||
+                'microphone ' + (audioSelect.length + 1);
+            option.text = audioLable;
+            audioSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+            const videoLabel = deviceInfo.label || 'camera ' +
+                (videoSelect.length + 1);
+            option.text = videoLabel;
+            videoSelect.appendChild(option);
+        } else {
+            console.log('Found one other kind of source/device: ', deviceInfo);
+        }
+    }
+}
+
+function getStream() {
+    if (window.stream) {
+        window.stream.getTracks().forEach(function(track) {
+            track.stop();
+        });
+    }
+    const constraints = {
+        audio: {
+            deviceId: {
+                exact: audioSelect.value
+            }
+        },
+        video: {
+            deviceId: {
+                exact: videoSelect.value
+            }
+        }
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
+}
+
+function handleSuccess(stream) {
+    webRtcTips.style.display = 'none';
+    videoElement.style.width = '100%';
+    videoElement.style.height = '100%';
+    window.stream = stream; // only to make stream available to console
+    videoElement.srcObject = stream;
+}
+
+function handleError(error) {
+    console.log('getUserMedia Error: ', error);
+}
--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 var path = require('path');
+var os = require("os");
+
+var networkInterfaces = os.networkInterfaces();
+
 const app = express();
 const port = 3000;
 
-// 加载静态资源
+// // 加载静态资源
 app.use(express.static(path.join(__dirname, './')));
 
-// 监听端口
+// // os 以太网的所有信息
+// // console.info(networkInterfaces);
+var ip = networkInterfaces['以太网'][1].address
+    // console.log(ip);
+    // // 监听端口
 app.listen(port, function(parmas) {
-    console.log('listen to http://0.0.0.0:' + port);
+    console.log('listen to http://' + ip + ':' + port);
 })
+var https = require('https'),
+    fs = require("fs");
+
+var options = {
+    key: fs.readFileSync('./privkey.pem'),
+    cert: fs.readFileSync('./server.pem')
+};
+
+https.createServer(options, app).listen(3011, function() {
+    console.log('Https server listening on port ' + 3011);
+});
